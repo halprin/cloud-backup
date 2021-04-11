84d481e544d13d4008e915600bcb6a8089e079a4
halprin <halprin@users.noreply.github.com>
2021-04-11 15:39:57 -0600

    Read and write test files

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"github.com/halprin/cloud-backup-go/crypt"
+	"io/ioutil"
+	"log"
+	"os"
+)
 
+func main() {
+	plaintext, err := ioutil.ReadFile(os.Args[1])
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	cyphertext := crypt.Encrypt(plaintext, "context string")
+
+	err = ioutil.WriteFile(os.Args[2], cyphertext, 0777)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
 }
---
+++ crypt/encryption.go
@@ -0,0 +1,5 @@
+package crypt
+
+func Encrypt(contents []byte, context string) []byte {
+	return nil
+}
