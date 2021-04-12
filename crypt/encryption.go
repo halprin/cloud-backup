3cf09fee4c17ffa5c9ced5c38f0a9d338de22b61
halprin <halprin@users.noreply.github.com>
2021-04-11 18:00:45 -0600

    Encryption!  Does it work?

--- crypt/encryption.go
+++ crypt/encryption.go
@@ -1,20 +1,87 @@
 package crypt
 
+import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/gob"
+)
+
 type envelope struct {
 	Key     []byte
+	Nonce   []byte
 	Message []byte
 }
 
-func Encrypt(contents []byte) ([]byte, error) {
+func Encrypt(plaintext []byte) ([]byte, error) {
 
 	dataKey, err := getEncryptionKey()
 	if err != nil {
 		return nil, err
 	}
 
-	messageEnvelope := &envelope{
-		Key: dataKey.encryptedDataKey,
+	encryptor, err := createAuthenticatedEncryption(dataKey)
+	if err != nil {
+		return nil, err
 	}
 
-	return nil, nil
+	clearPlaintextDataKey(dataKey)
+
+	nonce, err := generateNonce(encryptor)
+	if err != nil {
+		return nil, err
+	}
+
+	ciphertext := encryptor.Seal(nil, nonce, plaintext, []byte("a test context string"))
+
+	messageEnvelope := &envelope{
+		Key:     dataKey.encryptedDataKey,
+		Nonce:   nonce,
+		Message: ciphertext,
+	}
+
+	envelopeCipherText := &bytes.Buffer{}
+
+	err = gob.NewEncoder(envelopeCipherText).Encode(messageEnvelope)
+	if err != nil {
+		return nil, err
+	}
+
+	return ciphertext, nil
+}
+
+func createAuthenticatedEncryption(dataKey *dataKey) (cipher.AEAD, error) {
+	aesCipher, err := aes.NewCipher(dataKey.plaintextDataKey)
+	if err != nil {
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCM(aesCipher)
+	if err != nil {
+		return nil, err
+	}
+
+	return gcm, nil
+}
+
+func clearPlaintextDataKey(dataKey *dataKey)  {
+	plaintextDataKey := dataKey.plaintextDataKey
+
+	for index := 0; index < len(dataKey.plaintextDataKey); index++ {
+		plaintextDataKey[index] = 0
+	}
+
+	dataKey.plaintextDataKey = nil  //sets the key to nil which will designate the data to be released via the garbage collector
+}
+
+func generateNonce(aead cipher.AEAD) ([]byte, error) {
+	nonce := make([]byte, aead.NonceSize())
+
+	_, err := rand.Read(nonce)
+	if err != nil {
+		return nil, err
+	}
+
+	return nonce, nil
 }
