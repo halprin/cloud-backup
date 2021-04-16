23b6d7c7d748cf59f77046cf62933e2ee671f754
halprin <halprin@users.noreply.github.com>
2021-04-15 18:38:22 -0600

    Clean-up Archiver

--- archival/archive.go
+++ archival/archive.go
@@ -2,7 +2,6 @@ package archival
 
 import (
 	"archive/tar"
-	"bytes"
 	"io"
 	"io/fs"
 	"os"
@@ -39,7 +38,7 @@ func (receiver *archiver) Archive() error {
 			return err
 		}
 
-		err = writeFileMetadataToTar(relativePath, fileMetadata, receiver.tarWriter)
+		err = receiver.writeFileMetadataToTar(relativePath, fileMetadata)
 		if err != nil {
 			return err
 		}
@@ -50,7 +49,7 @@ func (receiver *archiver) Archive() error {
 		}
 
 		//it's an actual file
-		err = writeFileToTar(currentPath, receiver.tarWriter)
+		err = receiver.writeFileToTar(currentPath)
 		return err
 	})
 
@@ -66,63 +65,7 @@ func (receiver *archiver) Writer() io.Writer {
 	return receiver.tarWriter
 }
 
-func Archive(filePath string) ([]byte, error) {
-	parentDirectoryPath := filepath.Dir(filePath)
-
-	byteBuffer := bytes.Buffer{}
-	tarWriter := tar.NewWriter(&byteBuffer)
-	defer tarWriter.Close()
-
-	err := filepath.WalkDir(filePath, func(currentPath string, fileMetadata fs.DirEntry, err error) error {
-		if err != nil {
-			return err
-		}
-
-		//get the relative path the tar file doesn't have the entire absolute path but just the relative path from the start of the walk down to the current file
-		relativePath, err := filepath.Rel(parentDirectoryPath, currentPath)
-		if err != nil {
-			return err
-		}
-
-		err = writeFileMetadataToTar(relativePath, fileMetadata, tarWriter)
-		if err != nil {
-			return err
-		}
-
-		if fileMetadata.IsDir() {
-			//we're done if this is just a directory
-			return nil
-		}
-
-		//it's an actual file
-		err = writeFileToTar(currentPath, tarWriter)
-		return err
-	})
-
-	if err != nil {
-		return nil, err
-	}
-
-	err = tarWriter.Close()
-	if err != nil {
-		return nil, err
-	}
-
-	return byteBuffer.Bytes(), nil
-}
-
-func writeFileToTar(currentPath string, tarWriter *tar.Writer) error {
-	fileData, err := os.Open(currentPath)
-	if err != nil {
-		return err
-	}
-	defer fileData.Close()
-
-	_, err = io.Copy(tarWriter, fileData)
-	return err
-}
-
-func writeFileMetadataToTar(relativePath string, fileMetadata fs.DirEntry, tarWriter *tar.Writer) error {
+func (receiver *archiver) writeFileMetadataToTar(relativePath string, fileMetadata fs.DirEntry) error {
 	fileInfo, err := fileMetadata.Info()
 	if err != nil {
 		return err
@@ -134,6 +77,17 @@ func writeFileMetadataToTar(relativePath string, fileMetadata fs.DirEntry, tarWr
 	}
 	tarHeader.Name = relativePath //because fs.FileInfo's Name method only returns the base name
 
-	err = tarWriter.WriteHeader(tarHeader)
+	err = receiver.tarWriter.WriteHeader(tarHeader)
+	return err
+}
+
+func (receiver *archiver) writeFileToTar(currentPath string) error {
+	fileData, err := os.Open(currentPath)
+	if err != nil {
+		return err
+	}
+	defer fileData.Close()
+
+	_, err = io.Copy(receiver.tarWriter, fileData)
 	return err
 }
--- cmd/main.go
+++ cmd/main.go
@@ -5,31 +5,13 @@ import (
 	"github.com/halprin/cloud-backup-go/archival"
 	"github.com/halprin/cloud-backup-go/compression"
 	"github.com/halprin/cloud-backup-go/crypt"
-	"io/fs"
 	"log"
 	"os"
 )
 
 func main() {
-	//archiveAndCompressAndEncrypt()
+	archiveAndCompressAndEncrypt()
 	decrypt()
-}
-
-func archiveAndCompress() {
-	tarData, err := archival.Archive(os.Args[1])
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-
-	gzipData, err := compression.Compress(tarData)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-
-	err = os.WriteFile(os.Args[2], gzipData, fs.FileMode(777))
-	if err != nil {
-		log.Fatal(err.Error())
-	}
 }
 
 func archiveAndCompressAndEncrypt() {
@@ -58,23 +40,6 @@ func archiveAndCompressAndEncrypt() {
 	}
 
 	err = bufferedWriter.Flush()
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-}
-
-func encrypt() {
-	plaintext, err := os.ReadFile(os.Args[1])
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-
-	ciphertext, err := crypt.Encrypt(plaintext)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-
-	err = os.WriteFile(os.Args[2], ciphertext, 0777)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
