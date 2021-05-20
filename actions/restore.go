864cb15dbe091aa5ec109fd4243876ab45035f80
halprin <halprin@users.noreply.github.com>
2021-05-19 21:04:35 -0600

    Restore with decrypt

--- actions/restore.go
+++ actions/restore.go
@@ -1,9 +1,46 @@
 package actions
 
-import "log"
+import (
+	"github.com/halprin/cloud-backup-go/config"
+	"github.com/halprin/cloud-backup-go/crypt"
+	"github.com/halprin/cloud-backup-go/transfer"
+	"log"
+	"os"
+	"path/filepath"
+)
 
 func Restore(configFilePath string, timestamp string, backupFile string, restorePath string) error {
-	log.Println("Restoring file")
-	log.Printf("configFilePath=%s, timestamp=%s, backupFile=%s, restorePath=%s", configFilePath, timestamp, backupFile, restorePath)
+	overallConfig, err := config.New(configFilePath)
+	if err != nil {
+		return err
+	}
+
+	log.Printf("Restoring file %s from %s to %s", backupFile, timestamp, restorePath)
+
+	downloader, err := transfer.NewDownloader(overallConfig, timestamp, backupFile)
+	if err != nil {
+		return err
+	}
+
+	downloadReader, err := downloader.Download()
+	if err != nil {
+		return err
+	}
+
+	outputFilePath := filepath.Join(restorePath, backupFile + ".tar.gz")
+	outputFile, err := os.Create(outputFilePath)
+	if err != nil {
+		return err
+	}
+	defer outputFile.Close()
+
+	decryptor := crypt.NewDecryptor(downloadReader, outputFile, overallConfig)
+
+	err = decryptor.Decrypt()
+	if err != nil {
+		return err
+	}
+
+	log.Println("Restoring file complete")
 	return nil
 }
---
+++ transfer/download.go
@@ -0,0 +1,48 @@
+package transfer
+
+import (
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/halprin/cloud-backup-go/config"
+	"io"
+	"log"
+)
+
+type downloader struct {
+	s3Client    *s3.S3
+	s3Bucket    string
+	s3ObjectKey string
+}
+
+func NewDownloader(overallConfig config.BackupConfiguration, timestamp string, backupFile string) (*downloader, error) {
+	awsSession, err := session.NewSessionWithOptions(session.Options{
+		Profile: overallConfig.AwsProfile,
+	})
+	if err != nil {
+		log.Println("Initial AWS session failed")
+		return nil, err
+	}
+
+	s3ObjectKey := fmt.Sprintf("%s/%s", timestamp, backupFile)
+
+	return &downloader{
+		s3Client:    s3.New(awsSession),
+		s3Bucket:    overallConfig.S3Bucket,
+		s3ObjectKey: s3ObjectKey,
+	}, nil
+}
+
+func (receiver *downloader) Download() (io.Reader, error) {
+	getObjectInput := &s3.GetObjectInput{
+		Bucket: &receiver.s3Bucket,
+		Key:    &receiver.s3ObjectKey,
+	}
+
+	getObjectOutput, err := receiver.s3Client.GetObject(getObjectInput)
+	if err != nil {
+		return nil, err
+	}
+
+	return getObjectOutput.Body, nil
+}
